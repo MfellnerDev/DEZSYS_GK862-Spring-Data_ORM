2b0fc1e0550a6fbb9f1300770945fb8b124c1c11
mfellner <mfellner@student.tgm.ac.at>
2024-04-09 15:01:57 +0200

    feat(GKü): adding all things for GKü

---
+++ accessing-data-mysql/src/test/java/com/fellner/accessingdatamysql/AccessingDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fellner.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
